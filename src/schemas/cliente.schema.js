47ea89a43ef503319344b4d5df6613ed5874e49e
MoisesNunez435 <=>
2025-10-28 22:00:19 -0600

    Agregando Schema

---
+++ src/schemas/cliente.schema.js
@@ -0,0 +1,34 @@
+import { z } from 'zod';
+
+const createClienteSchema = z.object({
+    nombres: z.string()
+        .min(2, {message: 'Debe contener al menos 2 caracteres'})
+        .max(50, { message: 'No debe sobrepasar los 50 caracteres'} )
+        .regex(
+            /^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s]+$/, {message: 'Solo se permiten letras y espacios'}
+        )
+        .trim(),
+    apellidos: z.string()
+        .min(2, {message: 'Debe contener al menos 2 caracteres'})
+        .max(50, { message: 'No debe sobrepasar los 50 caracteres'} )
+        .regex(
+            /^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s]+$/, {message: 'Solo se permiten letras y espacios'}
+        )
+        .trim(),
+    telefono: z.string()
+        .regex(/^\d{8}$/, {
+            message: 'El número debe tener exactamente 8 dígitos numéricos',
+        }),
+    usuarioId: z.integer
+});
+
+const updateClienteSchema = z.object({
+    nombres: z.string().min(1).optional(),
+    apellidos: z.string().min(1).optional(),
+    telefono: z.string().optional()
+});
+
+export const ClienteSchema = {
+    create: createClienteSchema,
+    update: updateClienteSchema
+};
