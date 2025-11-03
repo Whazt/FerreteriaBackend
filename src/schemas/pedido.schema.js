bf804e73f4f0c82765a7a56664c2ccc243dda9e1
MoisesNunez435 <=>
2025-11-02 22:25:19 -0600

    Arreglando schemas compra y pedido

--- src/schemas/pedido.schema.js
+++ src/schemas/pedido.schema.js
@@ -1,8 +1,16 @@
 import { z } from 'zod';
 
 const UpdateEstadoPedidoSchema = z.object({
+    clienteId: z
+    .coerce
+    .number({
+      required_error: "El cliente es obligatorio.",
+    })
+    .int("El ID del cliente debe ser un número entero.")
+    .min(1, "Debe seleccionar un cliente existente."),
+
     accion: z.enum(['pendiente', 'pagado', 'enviado', 'cancelado'], {
-    invalid_type_error: "La acción debe ser 'pemdiente' o 'pagado' o 'enviado' o 'cancelado'.",
+    invalid_type_error: "La acción debe ser 'pendiente' o 'pagado' o 'enviado' o 'cancelado'.",
     }).optional()
 });
 
