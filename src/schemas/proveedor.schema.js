44fcabd63e97c7581065f06105e512bec61f9cd4
MoisesNunez435 <=>
2025-11-02 19:45:44 -0600

    Arreglando schema proveedor

--- src/schemas/proveedor.schema.js
+++ src/schemas/proveedor.schema.js
@@ -15,15 +15,15 @@ const createProveedorSchema = z.object({
 
 const updateProveedorSchema = z.object({
  nombres: z.string().min(1).optional(),
+ telefono: z.string().regex(/^\d{8}$/, {
+            message: 'El número debe tener exactamente 8 dígitos numéricos',
+        }).optional(),
  email: z.string().
         email({ message: 'El correo no tiene un formato válido' })
         .min(5, { message: 'El correo es demasiado corto' })
         .max(50, { message: 'El correo no puede exceder 50 caracteres' })
         .trim()
-        .optional(),
-    telefono: z.string().regex(/^\d{8}$/, {
-            message: 'El número debe tener exactamente 8 dígitos numéricos',
-        }).optional()
+        .optional()
 });
 
 export const ProveedorSchema = {
